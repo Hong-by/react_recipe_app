361280071eb918252d144c456f2404118af9104c
Bye-Hong <hby033@naver.com>
2021-09-02 10:09:20 +0900

    bye-hong
    리액트 어렵다

--- src/App.js
+++ src/App.js
@@ -1,104 +1,25 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Header from './components/Header';
-
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Details from "./components/Details";
 
 import "./css/App.css";
-import "./css/reset.css"
-import Recipe from "./components/Recipe";
+import "./css/Reset.css";
 
-const App = () => {
-
-  //1. 계정 등록
-  const App_ID = "488923a7";
-  const App_Key = "e7aaf5d53c8f67557e24668c725dbe5f";
-
-  // 4. recipes 요청 결과 데이터 useState 정의
-  const [recipes, setRecipes] = useState([]);
-
-  //10. 검색 useState의 정의
-  const [search, setSearch] = useState("");
-
-  //15. 검색 문자열 useState 정의
-  const [query, setQuery] = useState("chicken");
-
-  // 5. useEffect 정의
-  useEffect(() => {
-    getRecipes();
-  }, [query]);
-
-  // "https://api.edamam.com/search?q=chicken&app_id=488923a7&app_key=e7aaf5d53c8f67557e24668c725dbe5f"
-
-
-  //3. API 비동기 연결(axios)
-  //axios 설치 : https://velog.io/@sss5793/axios-%EC%82%AC%EC%9A%A9%ED%95%B4%EB%B3%B4%EA%B8%B0-uuk5elxk88
-  //axios 사용 : https://dev.to/ms_yogii/useaxios-a-simple-custom-hook-for-calling-apis-using-axios-2dkj
-
-  const getRecipes = () => {
-    axios
-      .get(`https://api.edamam.com/search?q=${query}&app_id=${App_ID}&app_key=${App_Key}`)
-      .then((res) => {
-        //useEffect의 콜백 파라미터에 []를 사용하지 않으면 콘솔에 계속 찍히다가 오류가 난다.
-        // console.log(res.data.hits);
-        setRecipes(res.data.hits);
-      });
-  }
-
-  //11. input search 기능 함수 정의
-  const updateSearch = (e) => {
-    setSearch(e.target.value);
-    // console.log(search)
-  }
-
-  //14. onSubmit 실행 함수 정의
-  const getSearch = (e) => {
-    e.preventDefault();
-    setSearch("");
-    setQuery(search);
-  }
-
-
+function App() {
   return (
-    <div className="App">
-      <Header />
-
-      {/* 2. 검색창 UI 작성 */}
-      <form className="search-form" onSubmit={getSearch}>
-        <div className="center">
-          <input
-            type="text"
-            value={search}
-            placeholder="Search Recipe... "
-            onChange={updateSearch}
-          />
-
-          {/* 13. button에 submit 타입을 지정하면 클릭했을때 감싸는 form의 onSubmit 속성이 실행 */}
-          <button type="submit">
-            <i class="fa fa-search"></i>
-          </button>
-
-        </div>
-      </form>
-
-      {/* 7. recipe 컴포넌트 작성 및 임포트 */}
-      <div className="recipes">
-        {/* 8. Recipe 컴포넌트 반복 처리 */}
-        {/* 9. 컴포넌트에 Props */}
-        {recipes.map((recipe) => (
-          <Recipe
-            key={recipe.recipe.label}
-            title={recipe.recipe.label}
-            calories={recipe.recipe.calories}
-            img={recipe.recipe.image}
-            ingrs={recipe.recipe.ingredients}
-
-          />
-        ))}
-
+    <Router>
+      <div className='App'>
+        <Header />
+        {/* <Home /> */}
+        <Route path='/' exact component={Home} />
+        <Route path='/details' component={Details} />
       </div>
-    </div>
+    </Router>
   );
-};
+}
 
 export default App;
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React from "react";
+import "../css/Recipe.css";
+import { Link } from "react-router-dom";
 
+//hooks
+//graphQL
 
 const Recipe = ({ title, calories, img, ingrs }) => {
-
-
+  //procs로 받아온 title을 뿌릴 수 있음
   return (
     <div>
-      <h2>{title}</h2>
+      <img src={img} alt='' />
+      <h2>
+        <Link
+          to={{
+            pathname: "/details",
+            state: {
+              title: title,
+              calories: calories,
+              img: img,
+              ingrs: ingrs,
+            },
+          }}>
+          {title}
+        </Link>
+      </h2>
       <ul>
-        {ingrs.map((ingr, i) => (<li key={i}>{ingr.text}</li>))}
+        {ingrs.map((ingr, i) => (
+          <li key={i}>{ingr.text}</li>
+          // i를 넣어준 것은 key값으로 넣어서 각각을 인식시켜줄수 없어서 i를 써서 구분시켜줬다.반복문으로 1,2,3 이렇게 가므로
+        ))}
       </ul>
-      <p>칼로리 : {calories}</p>
-      <img src={img} alt="" />
+      <p>{calories}</p>
     </div>
   );
 };
